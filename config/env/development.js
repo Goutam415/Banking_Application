8e8ebd482e8e4e853955e8a3fb76b9490108f4a3
Goutam <desaigoutam4@gmail.com>
2017-05-19 09:12:48 -0700

    changed database name.

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@ var defaultEnvConfig = require('./default');
 var domainName = 'http://localhost:';
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/notes_share',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/Banking',
     options: {
       user: '',
       pass: ''
